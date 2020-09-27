bd86ab3907958fc18814ff7b175f6ac42b4419ee
Vinícius Duarte da Silva <vinids@MacBook-Pro-de-Vinicius.local>
2020-09-27 16:16:28 -0300

    commit

--- src/components/Layout/Head.js
+++ src/components/Layout/Head.js
@@ -3,7 +3,7 @@ import NextHead from 'next/head';
 export default function Head() {
    return (
       <NextHead>
-        <title>Bruna e Vinícius</title>
+        <title>vini</title>
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no, user-scalable=0" />
 
         <link rel="icon" href="/static/favicon-32x32.png" sizes="32x32" />
